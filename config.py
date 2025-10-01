7be396bacff131f19d870d98209b8f5dcecd17e8
junaid Abdrahiman <junaidabdrahiman15@gmail.com>
2025-10-01 12:45:06 +0530

    update confiq

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 class Settings(BaseSettings):
     # Use SQLite for development by default
     # To use PostgreSQL, set DATABASE_URL environment variable to: postgresql://postgres:password@localhost:5432/dubai_travel
-    database_url: str = "postgresql://dubai_traval_agency:12345@db.xxxxx.render.com:5432/dubai_travel_db"
+    database_url: str = "postgresql://dubai_traval_agency:12345@render:5432/dubai_travel_db"
     secret_key: str = "dubai_travel_agency"
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
